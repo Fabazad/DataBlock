5d0fbd273bdccc1fb2f08b3218e9e0cc5d490130
Johan Brunet <brunet.johan.23@gmail.com>
2019-01-23 11:32:42 +0100

    [add] Facebook scripts

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -13,3 +13,21 @@ $("#go-to-google-ads").click((event)=>{
 		chrome.runtime.sendMessage({action: "goToGoogleAds", toDisable: toDisable});
 	 });
 });
+
+$("#stopTreatments").click((event)=>{
+	chrome.tabs.query({active: true, currentWindow: true}, function (tabs){
+		chrome.tabs.sendMessage(tabs[0].id, {action: "stopTreatments"});
+	 });
+})
+
+$("#deleteApps").click((event)=>{
+	chrome.tabs.query({active: true, currentWindow: true}, function (tabs){
+		chrome.tabs.sendMessage(tabs[0].id, {action: "deleteApps", deleteAll: false, url: "https://www.facebook.com/settings?tab=applications&section=inactive"});
+	 });
+})
+
+$("#deleteAllApps").click((event)=>{
+	chrome.tabs.query({active: true, currentWindow: true}, function (tabs){
+		chrome.tabs.sendMessage(tabs[0].id, {action: "deleteApps", deleteAll: true, url: "https://www.facebook.com/settings?tab=applications&section=inactive"});
+	 });
+})
